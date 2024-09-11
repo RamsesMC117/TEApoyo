c7ae26221bc5aef77e466e2acf35872ca4d53991
RamsesMC113 <158788680+RamsesMC113@users.noreply.github.com>
2024-09-11 17:42:40 -0600

    Modificacion del header.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,38 +1,39 @@
 import { Link } from "react-router-dom";
 import Logo from "../assets/logo-definitivo-removebg-preview.png";
 
-
 const Header = () => {
     return (
         <>
             <div className="h-screen w-full">
-                <nav className="flex items-center">
+                <div className="flex flex-row gap-60">
                     <img
                         className="w-52 m-2 p-1 cursor-pointer"
                         src={Logo} alt="Logo" />
-                    <ul className="flex-1 text-center">
-                        <li className="list-none inline-block px-5">
-                            <Link
-                                className="no-underline px-2"
-                                to="/">Inicio</Link>
-                        </li>
-                        <li className="list-none inline-block px-5">
-                            <Link
-                                className="no-underline px-2"
-                                to="/nosotros">Nosotros</Link>
-                        </li>
-                        <li className="list-none inline-block px-5">
-                            <Link
-                                className="no-underline px-2"
-                                to="/servicios">Servicios</Link>
-                        </li>
-                        <li className="list-none inline-block px-5">
-                            <Link
-                                className="no-underline px-2"
-                                to="/contacto">Contacto</Link>
-                        </li>
-                    </ul>
-                </nav>
+                    <nav className="flex items-center justify-center">
+                        <ul className="flex justify-center flex-1">
+                            <li className="list-none inline-block px-5">
+                                <Link
+                                    className="no-underline px-2"
+                                    to="/">Inicio</Link>
+                            </li>
+                            <li className="list-none inline-block px-5">
+                                <Link
+                                    className="no-underline px-2"
+                                    to="/nosotros">Nosotros</Link>
+                            </li>
+                            <li className="list-none inline-block px-5">
+                                <Link
+                                    className="no-underline px-2"
+                                    to="/servicios">Servicios</Link>
+                            </li>
+                            <li className="list-none inline-block px-5">
+                                <Link
+                                    className="no-underline px-2"
+                                    to="/contacto">Contacto</Link>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
             </div>
         </>
     );
